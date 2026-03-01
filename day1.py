42ea2e2c4abb3fc3146f9e3e7c19778d4e446939
24x7abdullah-glitch <24x7abdullah@gmail.com>
2026-03-01 13:15:42 +0600

    Refactor user input and output for personal info
    Updated user input prompts and improved output formatting. Changed birth year calculation to use a fixed current year.

--- day1.py
+++ day1.py
@@ -1,24 +1,21 @@
-print("i am learning python")
-print("lets get to know you")
+#User inputs
+user_name= input("What is your name? ")
+user_age= input("What is your age?(Please insert only numoric values) ")
+user_city= input("Which city are you from? " )
+user_occupation= input("What is your occupation? ")
 
-#user input
-name=input("what is your name? ")
-age= input("how old are you? ") 
-city=input("where do you live? ")
-job=input("What do you do? ")
+#calculations
+age= int(user_age)
+current_year= 2026
+birth_year= current_year - age
 
-# create a sammary 
-print("You summed up")
-print("Name "+ name)
-print("Age "+ age)
-print("city "+ city)
-print("job "+ job)
-print("............")
-
-# calculate birth year(approx)
-from datetime import datetime   
-current_year = datetime.now().year
-birth_year = current_year - int(age) 
-
-print(name+" you are probably born in "+ str(birth_year))
-print("welcome to the "+ city +" city, hope you can be a good "+job )
+#Results/summery
+print("---Personal Information---" )
+print("Name: "+user_name )
+print("Age: "+user_age )
+print("City: "+user_city )
+print("Occupation: "+user_occupation)
+print("------------")
+print(user_name +", You were born in "+ str(birth_year))
+print("Welcome to the " + user_city )
+print("Hope you are progressing as a "+ user_occupation)
